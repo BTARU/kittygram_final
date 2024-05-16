26d43d232bc081af0690e2c62e0c4abf895214f8
Bulat Ayupov <sulamar66@gmail.com>
2024-05-16 19:44:56 +0500

    settings fix 2

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -15,11 +15,18 @@ SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
 DEBUG = bool(os.getenv('DEBUG_SET', False))
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
-if ALLOWED_HOSTS:
-    ALLOWED_HOSTS = ALLOWED_HOSTS.split(', ')
-else:
-    ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
+# if ALLOWED_HOSTS:
+#     ALLOWED_HOSTS = ALLOWED_HOSTS.split(', ')
+# else:
+#     ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    '51.250.30.149',
+    'kitty-gram.sytes.net',
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
