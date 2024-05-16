6f0a470b17d5b78e47f10c2a3fc5ea342b8be774
Bulat Ayupov <sulamar66@gmail.com>
2024-05-16 17:51:21 +0500

    Fix after code review

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -3,22 +3,19 @@
 import os
 from pathlib import Path
 
+from django.core.management.utils import get_random_secret_key
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
-DEBUG = False
+DEBUG = bool(os.getenv('DEBUG_SET', False))
 
-ALLOWED_HOSTS = [
-    '127.0.0.1',
-    'localhost',
-    '51.250.30.149',
-    'kitty-gram.sytes.net',
-]
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -65,17 +62,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'django'),
-        'USER': os.getenv('POSTGRES_USER', 'django'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', ''),
-        'PORT': os.getenv('DB_PORT', 5432)
+DATABASES = os.getenv('DATABASE_SET')
+if DATABASES:
+    DATABASES = eval(DATABASES)
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
 # Password validation
 
